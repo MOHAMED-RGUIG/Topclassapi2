c0b6f23c43e6a33e7efd00784a7b7c98406497e2
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-08-26 16:21:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin:["*"],
+    origin:["https://topclass1.vercel.app"],
     methods:["POST","GET"],
     credentials:true}));
 
